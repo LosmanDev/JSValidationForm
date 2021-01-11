70eb9e36f14d838d03815ffb79af7cafff4bedf3
LosmanDev <losman78@gmail.com>
2021-01-11 18:53:42 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 let button = document.querySelector("#button");
+let clearBg1 = document.querySelector("#hide-after1");
+let clearBg2 = document.querySelector("#hide-after2");
 
 button.addEventListener("click", () => {
   let d = document.getElementById("myDialog");
@@ -8,6 +10,9 @@ button.addEventListener("click", () => {
   setInterval(function () {
     o += 0.01;
     d.style.opacity = o;
-    d.style.top = `${o * 5}%`;
+    d.style.top = o;
   }, 10);
+
+  clearBg1.classList.add("hide");
+  clearBg2.classList.add("hide");
 });
